cba242c16624dd4c5eb476f0b422158fa601649a
David Barocio <baropsx2@gmail.com>
2018-08-07 23:19:10 -0500

    3rd activity

--- ActividadIDR.py
+++ ActividadIDR.py
@@ -4,16 +4,32 @@ Created on 26/07/2018
 @author: David Barocio
 '''
 
-i=j=0
-while i <= 5:
-    while j <= 9:
-        print("El minuto es " + str(i) + str(j))
-        j+=1
-    j=0
-    i+=1
-    while i <= 5:
-        while j <= 9:
-            print("El segundo es " + str(i) + str(j))
-            j+=1
-        j=0
-        i+=1
+hora_1 = 0
+hora2 = 0
+min1 = 0
+min2 = 0
+seg1 = 0
+seg2 = 0
+
+while hora_1 <= 5:
+    while hora2 <= 9:
+        while min1 <= 5:
+            while min2 <= 9:
+                while seg1 <= 5:
+                    while seg2 <= 9:
+                        print("El tiempo es " + str(hora2) + ":" + str(min1) + str(min2) + ":" + str(seg1) + str(seg2))
+                        seg2+=1
+                    seg2=0
+                    seg1+=1
+                seg2=0
+                seg1=0
+                min2+=1
+            min2=0
+            min1+=1
+        min2=0
+        min1=0
+        hora2 +=1
+    hora2=0
+    hora_1 +=1
+    if hora2==1 & min1 == 0 & min2 == 0 & seg1 == 0 & seg2 == 0:
+        break
