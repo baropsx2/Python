0a035d4271b317af572463dceb1668514e1a7259
baropsx2 <baropsx2@gmail.com>
2018-09-04 11:03:51 -0500

    Actualización de código "actividad 3"

--- ActividadIDR.py
+++ ActividadIDR.py
@@ -3,33 +3,27 @@ Created on 26/07/2018
 
 @author: David Barocio
 '''
-
 hora_1 = 0
-hora2 = 0
 min1 = 0
 min2 = 0
 seg1 = 0
 seg2 = 0
 
-while hora_1 <= 5:
-    while hora2 <= 9:
-        while min1 <= 5:
-            while min2 <= 9:
-                while seg1 <= 5:
-                    while seg2 <= 9:
-                        print("El tiempo es " + str(hora2) + ":" + str(min1) + str(min2) + ":" + str(seg1) + str(seg2))
-                        seg2+=1
-                    seg2=0
-                    seg1+=1
-                seg2=0
-                seg1=0
-                min2+=1
-            min2=0
-            min1+=1
-        min2=0
-        min1=0
-        hora2 +=1
-    hora2=0
-    hora_1 +=1
-    if hora2==1 & min1 == 0 & min2 == 0 & seg1 == 0 & seg2 == 0:
-        break
+while min1 <= 5:
+    while min2 <= 9:
+        while seg1 <= 5:
+            while seg2 <= 9:
+                print("El tiempo es " + str(hora_1) + ":" + str(min1) + str(min2) + ":" + str(seg1) + str(
+                    seg2))
+                seg2 += 1
+            seg2 = 0
+            seg1 += 1
+        seg2 = 0
+        seg1 = 0
+        min2 += 1
+    min2 = 0
+    min1 += 1
+hora2 = 0
+hora_1 += 1
+
+print ("El tiempo es " + "01:00:00")
